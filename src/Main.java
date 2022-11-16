84bd00a5189a9bc7e340d20d031ca70ebe34a43b
dns <shulgaol@mail.ru>
2022-11-16 21:55:35 +0200

    задача 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 19;
+            if (age >= 18) {
+                System.out.println("Поздравляем, Вы - совершеннолетний!");
+        }
+            if (age < 18) {
+                System.out.println("Увы, до совершеннолетия Вам нужно подождать.");
+            }
+    }
+}
